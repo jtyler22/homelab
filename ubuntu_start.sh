e29a501d16d3fddd7610218359838e0e0de950e6
jtyler22 <41708607+jtyler22@users.noreply.github.com>
2019-06-17 08:42:10 -0700

    Update ubuntu_start.sh

--- ubuntu_start.sh
+++ ubuntu_start.sh
@@ -14,6 +14,13 @@ fi
 sudo ufw disable
 
 # Prep for Docker install
+sudo apt update
+sudo apt install \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
